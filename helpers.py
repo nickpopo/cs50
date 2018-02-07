afec640a49c2ff2acc0d0d8878e730e4aae23965
koyabica <koyabica@users.noreply.github.com>
2018-02-07 20:59:16 +0000

    20180207T205859Z

--- helpers.py
+++ helpers.py
@@ -1,15 +1,5 @@
 from nltk.tokenize import sent_tokenize
 
-# get string input and return list of substrings of string with lenght n
-def subs(string, n):
-    length = len(string)
-    s = []
-    for i in range(length):
-        if length - i < n:
-            break
-        s.append(string[i:n+i])
-    return s
-
 def lines(a, b):
     """Return lines in both a and b"""
 
@@ -38,3 +28,13 @@ def substrings(a, b, n):
     bSet = set(subs(b,n))
 
     return list(aSet & bSet)
+
+# get string input and return list of substrings of string with lenght n
+def subs(string, n):
+    length = len(string)
+    s = []
+    for i in range(length):
+        if length - i < n:
+            break
+        s.append(string[i:n+i])
+    return s
--- subhp.py
+++
@@ -1,9 +0,0 @@
-# get string input and return list of substrings of string with lenght n
-def subs(string, n):
-    length = len(string)
-    s = []
-    for i in range(length):
-        if length - i < n:
-            break
-        s.append(string[i:n+i])
-    return s
